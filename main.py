90fc752823b6eef7156ab608dd3f3467be655a98
DenisKhom <168731752+DenisKhom@users.noreply.github.com>
2024-05-11 18:56:57 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+# x=43
+# y=32
+# print(x*y)
+# print('End line')
+def test ():
+    a=1
+    b=2
+    print(a,b)
+def test2 ():
+    c=3
+    d=4
+    e=5
+    print(c,d,e)
+
+test2()
+
+test()
+test()
+test2()
